00b5bb7e299c79d9e16289a36200765c321a5852
Nuyoahwjl <3508529368@qq.com>
2024-09-25 17:18:59 +0800

    update

--- py/1.py
+++ py/1.py
@@ -259,11 +259,35 @@ a=1
 print(id(a))
 change(a)
 
-# 加了星号 * 的参数会以元组(tuple)的形式导入，存放所有未命名的变量参数。
+# 加了星号 * 的参数会以元组(tuple)的形式导入
 def printinfo( arg1, *vartuple ):
    "打印任何传入的参数"
    print ("输出: ")
    print (arg1)
    print (vartuple)
-# 调用printinfo 函数
 printinfo( 70, 60, 50 )
+# 加了两个星号 ** 的参数会以字典的形式导入
+def printinfo( arg1, **vardict ):
+   "打印任何传入的参数"
+   print ("输出: ")
+   print (arg1)
+   print (vardict)
+printinfo(1, a=2,b=3)
+
+a=[str(round(355/113, i)) for i in range(1, 6)]
+print(a)
+b=[round(355/113, i) for i in range(1, 6)]
+print(b)
+
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+print([[row[i] for row in matrix] for i in range(4)])
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+print(transposed)
